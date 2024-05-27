670fbc5bfa50efaeec56f71286c6bedf4151a473
Shiyun <serene.hoshiyun@gmail.com>
2024-05-27 19:43:49 +0800

    Fixed putendl

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: shiyun <shiyun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 14:25:48 by shiyun            #+#    #+#             */
-/*   Updated: 2024/05/27 14:52:14 by shiyun           ###   ########.fr       */
+/*   Updated: 2024/05/27 19:43:23 by shiyun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,9 @@
 */
 void	ft_putendl_fd(char *s, int fd)
 {
-    // size_t i;
-    // size_t strlen;
-    
-    // i = 0;
-    // strlen = ft_strlen(s);
-    if (s || fd > 0)
-    {
+    if (s != NULL)
+    {   
         ft_putstr_fd(s,fd);
-        write(1, "\n", 1);
+        ft_putchar_fd('\n', fd);
     }
 }
