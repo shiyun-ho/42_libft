733b1def2fcd537d737445687c478edc6737419a
Shiyun <serene.hoshiyun@gmail.com>
2024-06-01 13:42:19 +0800

    Added new files

---
+++ ft_strdup.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shiyun <shiyun@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 00:06:35 by shiyun            #+#    #+#             */
+/*   Updated: 2024/05/31 00:13:20 by shiyun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+* @desc: Returns a pointer to new string is a duplicate of string s
+*           Memory for new str is obtained with malloc
+*           Memory can be freed with free
+* @return: SUCCESS -> returns pointer to duplicated string
+            If memory is insufficient ->return NULL (with errno set to indicate error)
+*/
+char    *ft_strdup(const char *s)
+{
+    //Allocate memory for new string
+    //Duplicate string into memory allocated
+    //SUCCESS > return pointer to duplicated string
+    //INSUFFICIENT MEM > return NULL
+    //Free memory 
+    //return (ptr to duplicated string)
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: shiyun <shiyun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 11:17:03 by hshi-yun          #+#    #+#             */
-/*   Updated: 2024/05/30 23:57:16 by shiyun           ###   ########.fr       */
+/*   Updated: 2024/05/31 00:07:46 by shiyun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <unistd.h>
 # include <stddef.h>
 
-int	ft_isalpha(int c);
+int ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int ft_isascii(int c);
@@ -38,6 +38,8 @@ int ft_memcmp(const void *s1, const void *s2, size_t n);
 char *ft_strnstr(const char *big, const char *little, size_t len);
 int ft_atoi(const char *str);
 void    *ft_calloc(size_t n, size_t size);
+char    *ft_strdup(const char *s);
+
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
