48602db33cc9b81425bd3a1a76373d995d85908a
Shiyun <serene.hoshiyun@gmail.com>
2024-06-01 14:18:55 +0800

    Completed and passed tests for strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shiyun <shiyun@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 00:06:35 by shiyun            #+#    #+#             */
-/*   Updated: 2024/05/31 00:13:20 by shiyun           ###   ########.fr       */
+/*   Updated: 2024/06/01 14:17:25 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "libft.h"
 /*
 * @desc: Returns a pointer to new string is a duplicate of string s
 *           Memory for new str is obtained with malloc
@@ -19,10 +20,19 @@
 */
 char    *ft_strdup(const char *s)
 {
-    //Allocate memory for new string
-    //Duplicate string into memory allocated
-    //SUCCESS > return pointer to duplicated string
-    //INSUFFICIENT MEM > return NULL
-    //Free memory 
-    //return (ptr to duplicated string)
+    char    *s2;
+
+    s2 = malloc((ft_strlen(s) + 1) * sizeof(char));
+    if(!s2)
+        return (NULL);
+    ft_strlcpy(s2, s, ft_strlen(s) + 1);
+    return (s2);
 }
+// #include <stdio.h>
+
+// int main(void)
+// {
+
+// 	printf("%s", ft_strdup("hello"));
+// 	return (0);
+// }
