06066eaacf4f5f1629148815f558896f570db221
Shiyun <serene.hoshiyun@gmail.com>
2024-06-02 23:40:23 +0800

    Trying strtrimn

--- ft_strtrim_ref.c
+++ ft_strtrim_ref.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shiyun <shiyun@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 17:36:40 by hshi-yun          #+#    #+#             */
-/*   Updated: 2024/06/02 23:35:45 by shiyun           ###   ########.fr       */
+/*   Updated: 2024/06/01 22:47:57 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,56 @@
 * @return: trimmed str || allocation fails > NULL
 */
 
+int		start_index(char const *s1, const char *set)
+{
+	size_t	s1_len;
+	size_t	i;
 
+	s1_len = ft_strlen(s1);
+	i = 0;
+	while (i < s1_len)
+	{
+		if (ft_strchr(set, s1[i]) == 0)
+			break;
+		i++;
+	}
+	return (i);
+}
+int		end_index(char const *s1, const char *set)
+{
+	size_t	s1_len;
+	size_t	i;
+
+	s1_len = ft_strlen(s1);
+	i = 0;
+	while (i < s1_len)
+	{
+		if (ft_strchr(set, s1[s1_len - 1 - i]) == 0)
+			break;
+		i++;
+	}
+	return (s1_len - i);
+}
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
 	
-	if (s1 == NULL)
+	if (!s1)
 		return (NULL);
-	if (set == NULL)
+	if (!set)
 		return (ft_strdup(s1));
-	int left_ptr = 0;
-	int right_ptr = ft_strlen(s1) - 1; //points to end index
-	//First find where left_ptr will end in terms of last occurrence of set in s1
-	size_t i = 0;
-	while (i < ft_strlen(s1))
-	{
-		while (ft_strrchr(set, s1[i]) == NULL) 
-			break;
-		i++;
-	}
-	right_ptr = i;
-	i = right_ptr;
-	while (i > 0)
-	{
-		while (ft_strrchr(set, s1[i]) == NULL) 
-			break;
-		i--;
-	}
-	left_ptr = i;
-	if (right_ptr >= left_ptr)
+		
+	int start = start_index(s1, set);
+	int end = end_index(s1, set);
+
+	if (start >= end)
 		return (ft_strdup(""));
-	//Allocate memory using malloc
-	str = malloc(sizeof(char));
-	//if mem allocation fails, set return null
-	if (str == NULL)
-		return NULL;
-	//else, copy from s1[0] + left_ptr position, for r - l _+ 1 position
-	str = (char*) ft_strlcpy(str, s1 + left_ptr, right_ptr - left_ptr + 1);
-	//Return copy of s1 without chars in set at beginning and at the end of str
+	
+	str = malloc((end - start + 1) * sizeof(char));
+	if (str)
+	{
+		ft_strlcpy(str, s1+ start, end - start + 1);
+	}
 	return (str);
 }
 
